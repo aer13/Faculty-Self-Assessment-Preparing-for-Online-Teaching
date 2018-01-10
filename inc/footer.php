ba948b659bca983de465b323f519832479da9143
aer13 <aer13@psu.edu>
2018-01-10 13:22:57 -0500

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -15,9 +15,9 @@
             </div>
             <div class="col-sm-4">
                 <div class="text-center">
-                    <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img
+                    <a target="_blank" rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><img
                             alt="Creative Commons License" style="border-width:0"
-                            src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"/></a>
+                            src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/></a>
                 </div>
             </div>
 
@@ -30,7 +30,7 @@
             <span class="center-to-right pull-right text-right">
                <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">This teaching assessment</span> by <span
                     xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Penn State University</span> is licensed under a <a
-                    target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+                    target="_blank" rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
             </span>
                 </small>
             </div>
